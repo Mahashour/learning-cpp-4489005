a1d3190d1121ac1d03e117b4df2b84ff0e0a640d
Mahashour <mahmoud.a.ashour10@gmail.com>
2026-08-12 11:49:11 +0000

    Chapter 2 part 5

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -6,7 +6,22 @@
 #include <typeinfo>
 
 int main(){
-    
+    auto a = 8;
+    auto b = 8192831063;
+    auto c = true;
+    auto d = 2.72f;
+    auto e = 2.718281828459045;
+    auto f = '\n';
+    auto g = "hello world";
+
+    std::cout << "The type of a is " << typeid(a).name() << std::endl;
+    std::cout << "The type of b is " << typeid(b).name() << std::endl;
+    std::cout << "The type of c is " << typeid(c).name() << std::endl;
+    std::cout << "The type of d is " << typeid(d).name() << std::endl;
+    std::cout << "The type of e is " << typeid(e).name() << std::endl;
+    std::cout << "The type of f is " << typeid(f).name() << std::endl;
+    std::cout << "The type of g is " << typeid(g).name() << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
